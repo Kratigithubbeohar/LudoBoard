478f33438b9eaeb62a0523682401fdce8f6fb3bc
kratigithubbeohar <kratibeohar@gmail.com>
2025-08-24 16:13:58 +0530

    update LudoBoard logic/UI

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -10,28 +10,38 @@ export default function LudoBoard(){
     // BETTER WAY TO DO IT..
 
     let [moves , setMoves] = useState({blue:0,red:0,green:0,yellow:0})
+    let [arr , setArr] = useState(['no moves']);
     let updateBlue =()=>{
-        setMoves({...moves,blue: moves.blue +=1});
-        console.log(moves);
+        setMoves((previous)=>{
+            return {...previous,blue:previous.blue+1}
+        });
+        arr.push('blue moves');
+        setArr(arr);
+        console.log(arr);
     }
     let updateRed =()=>{
-        setMoves({...moves,red: moves.red +=1});
-        console.log(moves);
+        setMoves((previous)=>{
+            return {...previous,red:previous.red+1}
+        });
+        
     }
     let updateGreen =()=>{
-        setMoves({...moves,green: moves.green +=1});
-        console.log(moves);
+        setMoves((previous)=>{
+            return {...previous,green:previous.green+1}
+        });
+        
     }
     //callBack
     let updateYellow =()=>{
         setMoves((previous)=>{
             return {...previous,yellow:previous.yellow+1}
         });
-        console.log(moves);
+        
     }
     return (
         <div style = {{ border:"2px solid black", width:"150px",height:"410px",borderRadius:"20px",boxShadow:"2px 2px 9px grey"}}>
             <p>Blue moves = {moves.blue}</p>
+            <p>{arr}</p>
             <button onClick = {updateBlue} style={{backgroundColor:"blue"}}>+1</button>
             <p>Red moves = {moves.red}</p>
             <button onClick = {updateRed} style={{backgroundColor:"red"}}>+1</button>
